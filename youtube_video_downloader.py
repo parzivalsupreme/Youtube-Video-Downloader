a45564eb22acc7824c833bedb55c68571bab5bd3
Elijah Caesar Macabenlar <65802000+parzivalsupreme@users.noreply.github.com>
2022-12-17 22:49:51 +0800

    Add files via upload

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,35 +1,44 @@
+#imports
 from pytube import YouTube
 import pyttsx3
 
-
+#Print a welcome message
 print("🔴Youtube Video Downloader🔴")
 print("")
 
-
+#Initialize the text-to-speech engine
 engine = pyttsx3.init ('sapi5')
+#Get the available voices
 voices = engine.getProperty('voices')
+#Set the voice to the second available voice
 VoiceGender = engine.setProperty('voice', voices[1].id)
 
+#Function to speak the given text
 def speak(texttospeech):
     engine.say(texttospeech)
     engine.runAndWait()
 
-
-def I_wish_to_have_a_female_friend():
+#Function to download the YouTube video
+def yt():
     
+#Loop indefinitely
  while True:
     
     try:
+        #Get the YouTube URL from the user
         url = input("YouTube URL: ")
+        #Create a YouTube object using the URL
         youtube_video = YouTube(url)
+        #Get the highest resolution stream available
         youtube_video = youtube_video.streams.get_highest_resolution()
+        #Download the video
         youtube_video.download()
         print("Download Success!")
         speak("download sucess!")
-        
+    #If an error occurs  
     except:
         print("Error!")
         speak("error!")
 
-
-I_wish_to_have_a_female_friend()
+#Call the yt function to start the program
+yt()
